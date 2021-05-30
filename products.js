ab32682faaa64876263c93192a53844c7f43fde0
Olaore Fouad <olaorefouad565@gmail.com>
2021-05-30 12:24:02 +0400

    setup: created setup for vuejs file, images and products.js

---
+++ products.js
@@ -0,0 +1,48 @@
+const products = [
+    {
+        id: 1001,
+        title: 'Dodge Hellcat Redeye',
+        description: 'This car is everything you can ever want, I been dey tell you, I\'m in love!',
+        price: 201019.00,
+        image: 'images/dodge.jpg',
+        availableInventory: 10,
+        rating: 5
+    },
+    {
+        id: 1002,
+        title: 'Toyota Camry',
+        description: 'Toyota!, always repping!.',
+        price: 129.00,
+        image: 'images/camry.jpg',
+        availableInventory: 8,
+        rating: 2
+    },
+    {
+        id: 1003,
+        title: 'Honda Accord',
+        description: 'Make e no be like say we no put you for list, sha do normal',
+        price: 28.00,
+        image: 'images/honda.jpg',
+        availableInventory: 10,
+        rating: 2
+    },
+    {
+        id: 1004,
+        title: 'Bentley Truck',
+        description: 'I never knew bentley had a truck sha, it was always dem small cars I been dey know ',
+        price: 2029.00,
+        image: 'images/bentley.jpg',
+        availableInventory: 2,
+        rating: 5
+    },
+    {
+        id: 1005,
+        title: 'Lamborghini Aventador',
+        description: 'It\'s a lambo for crying out loud!, wetin you want gangan?',
+        price: 29.00,
+        image: 'images/lambo.jpg',
+        availableInventory: 6,
+        rating: 4
+    },
+]
+
