b1d811c1e223a20b720d45a23cc242d2a3f77207
Olaore Fouad <olaorefouad565@gmail.com>
2021-06-03 13:41:58 +0400

    edited files

--- service-worker.js
+++ service-worker.js
@@ -22,11 +22,25 @@ self.addEventListener('install', (e) => {
     )
 })
 
+// self.addEventListener('fetch', (e) => {
+//     e.respondWith(
+//         caches.match(e.request).then(function(r) {
+//             console.log('Fetching resource');
+//             return r;
+//         })
+//     )
+// })
+
 self.addEventListener('fetch', (e) => {
     e.respondWith(
         caches.match(e.request).then(function(r) {
-            console.log('Fetching resource');
-            return r;
+            return r ||  fetch(e.request).then(function(response) {
+                return caches.open(cacheName).then(function(cache) {
+                    cache.put(e.request, response.clone())
+                    return response;
+                })
+            }
+            )
         })
     )
 })
